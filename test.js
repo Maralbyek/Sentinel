2c7338cec1945b421ee6acdce12c3fa2ba4f15e5
Maralbek Tilek <maralbektilek@gmail.com>
2026-09-20 01:25:27 +0800

    Remove JS collector/detector logic, preparing for python backend

--- test.js
+++
@@ -1,8 +0,0 @@
-const getProcesses = require('./src/collectors/processes');
-const { analyzeProcesses } = require('./src/rules/detectors');
-
-getProcesses().then(processes => {
-  const findings = analyzeProcesses(processes);
-  console.log(`Found ${findings.length} findings:`);
-  console.log(findings);
-});
